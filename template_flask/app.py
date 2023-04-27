f5b4684cc5bd7114c90538882dff4639c0f6ff0a
Dobinda-Alessandro <dobinda.alessandro@iisgalvanimi.edu.it>
2023-04-27 11:02:44 +0000

    2

---
+++ template_flask/app.py
@@ -0,0 +1,24 @@
+from flask import render_template
+from flask import Flask
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('hello.html', name='Alessandro')
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="MC_PANINI"
+)
+mycursor = mydb.cursor()
+
+
+@app.route('/units')
+def unitList():
+    mycursor.execute("Select * FROM MC_unit")
+    myresult = mycursor.fetchall()
+    return render_template('MC_GG.html', units=myresult)
